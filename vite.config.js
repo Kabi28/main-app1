225d105d73f195c08f8b8f079650eaca9537f39f
kabi28 <vskabilan2004@gmail.com>
2025-02-19 15:24:32 +0530

    h

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-redux']  // Add react-redux to the external list
+    }
+  }
+});
+
